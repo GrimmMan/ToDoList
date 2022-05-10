fc67063e044f84ba193f0829666b7381f375b219
GrimmMan <105287999+GrimmMan@users.noreply.github.com>
2022-05-10 23:40:03 +0300

    My first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+app.set('View engine', 'ejs');
+const path = require('path');
+const PORT = 3000;
+const createPath = (page) => path.resolve(__dirname, 'ejs-view',`${page}.ejs`);
+//const db = 'mongodb+srv://GrimmMan:<pas12345>@cluster0.kjis4.mongodb.net/Test?retryWrites=true&w=majority';
+//mongoose
+  //.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+  //.then((res) => console.log('Connect to DB'))
+  //.catch((error) => console.log(error));
+app.listen(PORT,(error) => {
+    error ? console.log(error) : console.log(`Listening port ${PORT}`);
+})
